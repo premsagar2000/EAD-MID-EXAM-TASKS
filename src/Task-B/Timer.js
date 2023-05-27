df6d470aef1167cd19d0ff076c397e310be02f1a
Prem Sagar <prem.bscsf19@iba-suk.edu.pk>
2023-05-27 17:11:26 +0500

    Add CountDown Functionality in Timer Component

--- src/Task-B/Timer.js
+++ src/Task-B/Timer.js
@@ -3,9 +3,19 @@ import { useEffect } from 'react';
 
 function Timer() {
     const [count, setCount] =useState(10);
-    
+
+    useEffect(() => {
+        if (count > 0) {
+            setTimeout(() => setCount(count - 1), 1000);
+        } 
+        else {
+            setCount('Time Up!');
+        }
+    });
+
+   
   return (
-   <div> Timer - {count} </div>
+   <div> {count} </div>
   )
 }
 
